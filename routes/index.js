d7487f3668a631ffedb3d895da687e0074b84240
MarcusBYUI <ojo18002@byui.edu>
2022-09-03 19:27:04 +0100

    added sms route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,20 @@ const route = require("express").Router();
 const Joi = require("joi");
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
+const Vonage = require("@vonage/server-sdk");
 const saltRounds = 1;
 
 const { connection } = require("../db/connect");
+
+const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
+const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
+const TO_NUMBER = process.env.VONAGE_TO_NUMBER;
+const VONAGE_BRAND_NAME = process.env.VONAGE_BRAND_NAME;
+
+const vonage = new Vonage({
+  apiKey: VONAGE_API_KEY,
+  apiSecret: VONAGE_API_SECRET,
+});
 
 route.post("/", async (req, res, next) => {
   const schema = Joi.object().keys({
@@ -77,6 +88,43 @@ route.post("/", async (req, res, next) => {
           res.status(200).send("update successfull");
       } catch (error) {
         next(error);
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+route.post("/message", async (req, res, next) => {
+  const schema = Joi.object().keys({
+    number: Joi.number().required(),
+  });
+
+  try {
+    const value = await schema.validateAsync({
+      number: req.body.number,
+    });
+
+    //prepare message
+    const from = VONAGE_BRAND_NAME;
+    const to = TO_NUMBER;
+    const text = `You have ${value.number} users awaiting League mint`;
+
+    //send text message
+
+    vonage.message.sendSms(from, to, text, (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (responseData.messages[0]["status"] === "0") {
+          res.status(200).send("successful.");
+        } else {
+          res
+            .status(422)
+            .send(
+              `Message failed with error: ${responseData.messages[0]["error-text"]}`
+            );
+        }
       }
     });
   } catch (error) {
